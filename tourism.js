2bf6265418fdfc19eac217a9f61c5e71e3cb9ef6
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-03-08 13:03:37 +0200

    lab6a

--- tourism.js
+++ tourism.js
@@ -1,23 +1,36 @@
 var order = prompt("Where would you like go ");
-var how = prompt("you wont go bus or car");
-while(how != 'car'&& how!='bus')
+    var how = prompt("you wont go bus or car");
+
+var choise= function()
 {
+    while(how != 'car'&& how!='bus')
+ { 
+    
     how = prompt("you wont go bus or car");
+ }
 }
- var boc ='';
+
+choise();
+ 
+ 
+var boc ='';
 if (how =="bus")
 {
- boc= '<img src="scanis-bus4.jpg"/>' ;
+ boc = '<img src="scanis-bus4.jpg"/>' ;
 
 }
 else{
     boc ='<img src="2018-Ford-Focus.jpg"/>' ;
 }
 var num =prompt("How many person");
-
-var result ='';
+function reslt()
+{   var result ='';
 for (var i=0;i<num;i++)
 {
     result= result+boc;
 }
-document.write(result);
+ document.write(result);
+} reslt();
+
+
+
